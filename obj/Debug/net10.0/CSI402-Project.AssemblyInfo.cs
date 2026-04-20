5a012164381ed08b29c18784b1bd26079e6f67e5
Teeraphan Thienpromthong <pigeonthienpromthong@gmail.com>
2026-04-20 17:29:13 +0700

    1

--- obj/Debug/net10.0/CSI402-Project.AssemblyInfo.cs
+++ obj/Debug/net10.0/CSI402-Project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CSI402-Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14a5aaf798001a8782c4ca968cbded56a786492a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aae0f84d3e9375d829f694efe443f5433c99f28f")]
 [assembly: System.Reflection.AssemblyProductAttribute("CSI402-Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CSI402-Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
